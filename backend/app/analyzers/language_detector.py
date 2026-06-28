9ac1d49bd5538fafd7cdc0320b1e421867df23ce
kojagori320 <kojagori.joyee@gmail.com>
2026-06-28 12:53:08 +0530

    Fixed language detector

--- backend/app/analyzers/language_detector.py
+++ backend/app/analyzers/language_detector.py
@@ -1,17 +1,45 @@
-SUPPORTED_EXTENSIONS = {
+import os
+
+
+EXTENSION_MAP = {
     ".py": "Python",
-    ".ipynb": "Jupyter Notebook",
     ".js": "JavaScript",
     ".jsx": "React JSX",
     ".ts": "TypeScript",
-    ".tsx": "React TSX",
+    ".tsx": "React TypeScript",
     ".java": "Java",
     ".cpp": "C++",
     ".c": "C",
+    ".cs": "C#",
+    ".go": "Go",
+    ".rb": "Ruby",
+    ".php": "PHP",
     ".html": "HTML",
     ".css": "CSS",
+    ".scss": "SCSS",
     ".json": "JSON",
+    ".xml": "XML",
+    ".ipynb": "Jupyter Notebook",
     ".md": "Markdown",
-    ".yaml": "YAML",
-    ".yml": "YAML"
+    ".sql": "SQL",
+    ".sh": "Shell Script"
 }
+
+
+def detect_languages(project_path):
+
+    detected_languages = set()
+
+    for root, dirs, files in os.walk(project_path):
+
+        for file in files:
+
+            extension = os.path.splitext(file)[1].lower()
+
+            if extension in EXTENSION_MAP:
+
+                detected_languages.add(
+                    EXTENSION_MAP[extension]
+                )
+
+    return sorted(list(detected_languages))
